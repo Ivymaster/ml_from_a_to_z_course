204798a1222af2865c9907fd766095b8627b762a
ivymaster <ivan.ivusic.983@gmail.com>
2021-11-17 01:07:54 +0100

    Added the feature scaling(standardisation) to the dataset

--- my_code/1_data_preprocessing/first.py
+++ my_code/1_data_preprocessing/first.py
@@ -35,4 +35,13 @@ from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
 
-print(y)
+#split the data into training and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
+print(X_train)
